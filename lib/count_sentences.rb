a61ecfcc8f240176d9faf8d8e86eeb3c215a1b6b
Kelin Field <kfield111@hotmail.com>
2020-08-14 00:12:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     variable = self.split(/[.?!]/)
-    variable.reject {|temp| temp.empty?}
+    variable.delete(" ")
     variable.count
     binding.pry
   end
